d9f4d07e5da918e4a93979e9a80339ac33053001
6six7sven <chuzzamenhowasigna@gmail.com>
2021-01-13 14:08:00 +0530

    webhookping-2

--- bot.py
+++ bot.py
@@ -146,6 +146,7 @@ async def on_message(message):
     if message.content.startswith('!inspire'):
         quote = get_quote()
         await message.channel.send(quote)
+    
     if any(word in message.content for word in sad_words):
         await message.channel.send(random.choice(starter_encouragements))
     if any(word in message.content for word in happy_words):
